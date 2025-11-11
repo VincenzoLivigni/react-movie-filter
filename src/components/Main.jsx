34f9ceda3f80dd125d4baad1d08ddb8c09f94560
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-11 16:50:20 +0100

    Add a search field to filter movies by tite

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,8 +11,21 @@ export default function Main() {
         { title: 'Pulp Fiction', genre: 'Thriller' },
     ];
 
-    const [genereSelezionato, setGenereSelezionato] = useState("")
-    const [filmFiltrati, setFilmFiltrati] = useState(listaFilm)
+    function handleSearch(e) {
+        e.preventDefault();
+        console.log(filmFiltrati);
+    }
+
+
+    const [genereSelezionato, setGenereSelezionato] = useState("");
+    const [filmFiltrati, setFilmFiltrati] = useState(listaFilm);
+    const [cerca, setCerca] = useState("");
+
+    useEffect(() => {
+        const ricercaFiltrata = listaFilm.filter((film) => film.title.toLowerCase().includes(cerca.toLowerCase()))
+        setFilmFiltrati(ricercaFiltrata)
+    }, [cerca])
+
 
     useEffect(() => {
         if (genereSelezionato === "") {
@@ -23,9 +36,9 @@ export default function Main() {
             const listaFiltrata = listaFilm.filter((film) => film.genre === genereSelezionato)
             setFilmFiltrati(listaFiltrata)
             console.log(listaFiltrata);
-
         }
     }, [genereSelezionato])
+
     return (
         <main>
 
@@ -33,8 +46,15 @@ export default function Main() {
                 <div className="col-6 pt-5 mx-auto">
                     <h1 className="display-5"></h1>
                     <div className="card p-3 bg-primary">
-                        <form>
-                            <h3 className="pb-2 text-center text-white">Scegli un genere</h3>
+                        <form onSubmit={handleSearch}>
+
+                            <h3 className="pt-2 text-white">Ricerca per titolo</h3>
+                            <div className="d-flex py-2">
+                                <input value={cerca} onChange={(e) => setCerca(e.target.value)} className="form-control me-2" type="search" placeholder="Seleziona un film" />
+                                <button className="btn btn-outline-light" type="submit">Cerca</button>
+                            </div>
+
+                            <h3 className="pb-2 text-white">Scegli un genere</h3>
                             <div className="input-group mb-1">
                                 <select value={genereSelezionato} onChange={(e) => setGenereSelezionato(e.target.value)} className="form-select" aria-label="Default select example">
                                     <option value="">Seleziona un genere</option>
@@ -44,7 +64,9 @@ export default function Main() {
                                     <option value="Thriller">Thriller</option>
                                 </select>
                             </div>
+
                         </form>
+
                         <ul className="list-group mt-3">
                             {
                                 filmFiltrati.map((item, index) => (
@@ -52,6 +74,7 @@ export default function Main() {
                                 ))
                             }
                         </ul>
+
                     </div>
                 </div>
             </div>
