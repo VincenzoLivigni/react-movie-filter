7c1b3163d316629a1614dff76030c1f2d1369197
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-11 11:42:24 +0100

    first commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+
+    return (
+        <header className="bg-primary text-white py-3 text-center">
+            <h1>React movie filter</h1>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,8 @@
+export default function Main() {
+
+    return (
+        <main>
+            <h4>Main content</h4>
+        </main>
+    )
+}
