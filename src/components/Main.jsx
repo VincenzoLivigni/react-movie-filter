a0d873b88f45597e065ee61323d4f5090b15d37d
VincenzoLivigni <enzolivigni13@gmail.com>
2025-11-11 14:41:49 +0100

    Add dynamic movie filter using useState and useEffect

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from "react";
+
 export default function Main() {
 
     const listaFilm = [
@@ -9,28 +11,43 @@ export default function Main() {
         { title: 'Pulp Fiction', genre: 'Thriller' },
     ];
 
+    const [genereSelezionato, setGenereSelezionato] = useState("")
+    const [filmFiltrati, setFilmFiltrati] = useState(listaFilm)
+
+    useEffect(() => {
+        if (genereSelezionato === "") {
+            setFilmFiltrati(listaFilm)
+            console.log(listaFilm);
+
+        } else {
+            const listaFiltrata = listaFilm.filter((film) => film.genre === genereSelezionato)
+            setFilmFiltrati(listaFiltrata)
+            console.log(listaFiltrata);
+
+        }
+    }, [genereSelezionato])
     return (
         <main>
 
             <div className="container">
                 <div className="col-6 pt-5 mx-auto">
                     <h1 className="display-5"></h1>
-                    <div className="card p-3">
+                    <div className="card p-3 bg-primary">
                         <form>
-                            <h3 className="pb-2 text-center">Scegli un film</h3>
+                            <h3 className="pb-2 text-center text-white">Scegli un genere</h3>
                             <div className="input-group mb-1">
-                                <select className="form-select" aria-label="Default select example">
-                                    <option selected>Seleziona un genere</option>
-                                    <option value="1">Azione</option>
-                                    <option value="2">Fantascienza</option>
-                                    <option value="3">Romantico</option>
-                                    <option value="3">Thriller</option>
+                                <select value={genereSelezionato} onChange={(e) => setGenereSelezionato(e.target.value)} className="form-select" aria-label="Default select example">
+                                    <option value="">Seleziona un genere</option>
+                                    <option value="Azione">Azione</option>
+                                    <option value="Fantascienza">Fantascienza</option>
+                                    <option value="Romantico">Romantico</option>
+                                    <option value="Thriller">Thriller</option>
                                 </select>
                             </div>
                         </form>
                         <ul className="list-group mt-3">
                             {
-                                listaFilm.map((item, index) => (
+                                filmFiltrati.map((item, index) => (
                                     <li key={index} className="list-group-item d-flex justify-content-between align-items-center">{item.title}  ({item.genre})</li>
                                 ))
                             }
